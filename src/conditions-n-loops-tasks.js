21ce54937c6436cd288b5f07ebc7e52fe26e257b
Igor <igor@MacBook-Pro-Igor.local>
2023-12-31 15:04:31 +0200

    feat: add solution is Palindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -260,8 +260,12 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let strReverse = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    strReverse += str[i];
+  }
+  return str === strReverse;
 }
 
 /**
